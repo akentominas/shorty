4b1b41e22c1bdfc915267be795936cb4f944abf1
akentominas <akentominas@gmail.com>
2022-03-31 16:25:15 +0300

    renaming config settings for environment variables reading for url shortener providers hostnames

--- shorty/config/settings.py
+++ shorty/config/settings.py
@@ -10,11 +10,11 @@ from pathlib import PurePath
 CONFIG = {
     "environment": os.environ.get("SHORTY_DEPLOYMENT_ENVIRONMENT", "prod"),
     "bitly": {
-        "hostname": os.environ.get("BITLY_ENDPOINT", "https://api-ssl.bitly.com"),
+        "hostname": os.environ.get("BITLY_HOSTNAME", "https://api-ssl.bitly.com"),
         "access_token": os.environ.get("BITLY_TOKEN", "")
     },
     "tinyurl": {
-        "hostname": os.environ.get("TINY_ENDPOINT", "https://api.tinyurl.com"),
+        "hostname": os.environ.get("TINY_HOSTNAME", "https://api.tinyurl.com"),
         "access_token":  os.environ.get("TINY_TOKEN", "")
     },
 }
